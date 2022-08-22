7a2f74b9f5c8348a1586076c3d4ace0d199a2283
Aragas <personal@aragas.org>
2022-08-22 22:35:11 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ const CopyPlugin = require("copy-webpack-plugin");
 const config = webpack('game-mount-and-blade2', __dirname, 5);
 config.plugins.push(new CopyPlugin({
     patterns: [
-        { from: "**/Bannerlord.ModuleManager.Native.dll", to: "Bannerlord.ModuleManager.Native.dll" },
+        { from: "**/Bannerlord.ModuleManager.Native.dll", to: "[name].[ext]" },
+        { from: "src/*.jpg", to: "[name].[ext]" },
+        { from: "src/*.png", to: "[name].[ext]" },
     ],
 }));
 config.module.rules.push({
