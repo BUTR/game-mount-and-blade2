c2b880cdbf36157639499e21acf20052939fbcc2
Aragas <personal@aragas.org>
2022-08-22 22:02:36 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('vortex-api/bin/webpack').default;
 const CopyPlugin = require("copy-webpack-plugin");
 
 const config = webpack('game-mount-and-blade2', __dirname, 5);
-config.node.__dirname = false;
 config.plugins.push(new CopyPlugin({
     patterns: [
         { from: "**/Bannerlord.ModuleManager.Native.dll", to: "Bannerlord.ModuleManager.Native.dll" },
