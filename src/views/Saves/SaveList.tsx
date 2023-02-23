49150cf4f78b7c501955e20a7300c5fc0389437a
insomnious <simon.davies@nexusmods.com>
2023-02-23 16:31:36 +0000

    initial checkin. display of save data.

--- src/views/Saves/SaveList.tsx
+++ src/views/Saves/SaveList.tsx
@@ -1,108 +1,111 @@
 import * as React from 'react';
-import { Panel, ListGroup } from 'react-bootstrap';
-import { ComponentEx, FlexLayout, IconBar,  MainPage, ToolbarIcon, types } from 'vortex-api';
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ComponentEx, FlexLayout, IconBar, MainPage, ToolbarIcon, types } from 'vortex-api';
+import { types as vetypes } from '@butr/vortexextensionnative';
 import SaveEntry from './SaveListEntry';
 import { IItemRendererProps } from '../../types';
 import { VortexLauncherManager } from '../../utils/VortexLauncherManager';
-import { types as vetypes } from '@butr/vortexextensionnative';
 
-interface IStateProps {
-    
-};
+interface IStateProps {}
 type IOwnProps = IItemRendererProps & {
-    launcherManager: VortexLauncherManager;
+  launcherManager: VortexLauncherManager;
 };
 
 interface IBaseState {
-    saves: vetypes.SaveMetadata[];
-};
+  saves: vetypes.SaveMetadata[];
+}
 
 type IComponentProps = IStateProps & IOwnProps;
 type IComponentState = IBaseState;
 class SaveList extends ComponentEx<IComponentProps, IComponentState> {
+  private mStaticButtons: types.IActionDefinition[];
 
-    private mStaticButtons: types.IActionDefinition[];
+  constructor(props: IComponentProps) {
+    super(props);
 
-    constructor(props: IComponentProps) {
-        super(props);
+    const saves = props.launcherManager.getSaveFiles();
 
-        const saves = props.launcherManager.getSaveFiles();
+    this.initState({
+      saves,
+    });
 
-        this.initState({
-            saves: saves,
-        });
+    this.mStaticButtons = [
+      {
+        component: ToolbarIcon,
+        props: () => ({
+          id: `btn-refresh-list`,
+          key: `btn-refresh-list`,
+          icon: `refresh`,
+          text: `Refresh List`,
+          className: `load-order-refresh-list`,
+          onClick: this.onRefreshList,
+        }),
+      },
+    ];
+  }
 
-        this.mStaticButtons = [
-            {
-                component: ToolbarIcon,
-                props: () => {
-                    return {
-                        id: 'btn-refresh-list',
-                        key: 'btn-refresh-list',
-                        icon: 'refresh',
-                        text: 'Refresh List',
-                        className: 'load-order-refresh-list',
-                        onClick: this.onRefreshList,
-                    };
-                },
-            },
-        ];
-    }
+  public render(): JSX.Element {
+    const { t } = this.props;
 
-    public render(): JSX.Element {
-        const { t } = this.props;
+    return (
+      <MainPage>
+        <MainPage.Header>
+          <IconBar group="bannerlord-saves-icons" staticElements={this.mStaticButtons} className="menubar" t={t!} />
+        </MainPage.Header>
+        <MainPage.Body>{this.renderContent()}</MainPage.Body>
+      </MainPage>
+    );
+  }
 
-        return (
-        <MainPage>
-            <MainPage.Header>
-            <IconBar
-                group='bannerlord-saves-icons'
-                staticElements={this.mStaticButtons}
-                className='menubar'
-                t={t!}
-            />
-            </MainPage.Header>
-            <MainPage.Body>
-            {this.renderContent()}
-            </MainPage.Body>
-        </MainPage>
-        );
-    }
+  private renderContent() {
+    const { saves } = this.state;
 
-    private renderContent() {
-        const { saves } = this.state;
-        
-        return (
-            <Panel>
-              <Panel.Body>
-                <FlexLayout type='column'>
-                <FlexLayout.Flex>
-                    <ListGroup>
-                        {saves.map((item) => (
-                            <SaveEntry item={item} onSelected={this.onSaveSelected}></SaveEntry>
-                        ))}
-                    </ListGroup>
-                  </FlexLayout.Flex>
-                </FlexLayout>
-              </Panel.Body>
-            </Panel>
-        );
-    }
+    return (
+      <Panel>
+        <Panel.Body>
+          <FlexLayout type="column">
+            <FlexLayout.Flex>
+              <ListGroup>
+                <ListGroupItem
+                  style={{
+                    backgroundColor: `var(--brand-bg, black)`,
+                    borderBottom: `2px solid var(--border-color, white)`,
+                  }}
+                >
+                  <FlexLayout type="row">
+                    {/* TODO: How to FlexLayout.Flex? */}
 
-    private onRefreshList = () => {
-        const { launcherManager } = this.props;
+                    <FlexLayout.Flex>
+                      Name | ApplicationVersion | CharacterName | MainHeroLevel | DayLong | CreationTime
+                    </FlexLayout.Flex>
+                  </FlexLayout>
+                </ListGroupItem>
 
-        const saves = launcherManager.getSaveFiles();
-        this.setState({
-            saves: saves,
-        });
-    }
+                {saves.map((item) => (
+                  <SaveEntry item={item} onSelected={this.onSaveSelected}></SaveEntry>
+                ))}
+              </ListGroup>
+            </FlexLayout.Flex>
+          </FlexLayout>
+        </Panel.Body>
+      </Panel>
+    );
+  }
 
-    private onSaveSelected = (saveMetadata: vetypes.SaveMetadata) => {
-        const { launcherManager } = this.props;
+  private onRefreshList = () => {
+    const { launcherManager } = this.props;
 
-        launcherManager.setGameParameterSaveFile(saveMetadata.Name)
-    }
+    const saves = launcherManager.getSaveFiles();
+    this.setState({
+      saves,
+    });
+  };
+
+  private onSaveSelected = (saveMetadata: vetypes.SaveMetadata) => {
+    const { launcherManager } = this.props;
+
+    launcherManager.setGameParameterSaveFile(saveMetadata.Name);
+  };
 }
 
 export default SaveList;
--- src/views/Saves/SaveListEntry.tsx
+++ src/views/Saves/SaveListEntry.tsx
@@ -1,51 +1,50 @@
 import * as React from 'react';
-import { ListGroupItem } from 'react-bootstrap';
-import { ComponentEx } from 'vortex-api';
+import { Checkbox, Col, ListGroupItem, Radio, Row } from 'react-bootstrap';
+import { ComponentEx, FlexLayout } from 'vortex-api';
 import { types as vetypes } from '@butr/vortexextensionnative';
+import { Container } from 'react-bootstrap/lib/Tab';
 
-interface IStateProps {
-    
-};
+interface IStateProps {}
 type IOwnProps = {
-    item: vetypes.SaveMetadata;
-    onSelected: (saveMetadata: vetypes.SaveMetadata) => void
+  item: vetypes.SaveMetadata;
+  onSelected: (saveMetadata: vetypes.SaveMetadata) => void;
 };
 
-interface IBaseState {
-
-};
+interface IBaseState {}
 
 type IComponentProps = IStateProps & IOwnProps;
 type IComponentState = IBaseState;
 class SaveListEntry extends ComponentEx<IComponentProps, IComponentState> {
+  constructor(props: IComponentProps) {
+    console.log(`SaveListEntry`);
+    super(props);
+  }
 
-    constructor(props: IComponentProps) {
-        super(props);
-    }
+  public render(): JSX.Element {
+    const { item } = this.props;
 
-    public render() {
-        const { item } = this.props;
+    return (
+      <ListGroupItem
+        style={{
+          backgroundColor: `var(--brand-bg, black)`,
+          borderBottom: `2px solid var(--border-color, white)`,
+        }}
+      >
+        <FlexLayout type="row">
+          {/* TODO: How to FlexLayout.Flex? */}
 
-        return (
-            <ListGroupItem style={{
-                backgroundColor: 'var(--brand-bg, black)',
-                borderBottom: '2px solid var(--border-color, white)'
-            }}>
-                <div style={{
-                    fontSize: '1.1em',
-                }}>
-                    <div>{item.Name}</div>
-                    {/*
-                    <div>{item.ApplicationVersion ?? 'Save Old'}</div>
-                    <div>{item.CharacterName ?? 'Save Old'}</div>
-                    <div>{item.MainHeroLevel ?? 'Save Old'}</div>
-                    <div>{item.DayLong ?? 'Save Old'}</div>
-                    <div>{item.CreationTime ?? 'Save Old'}</div>
-                    */}
-                </div>
-            </ListGroupItem>
-        );
-    }
+          <FlexLayout.Flex>
+            <div style={{ display: `flex`, alignItems: `center` }}>
+              <Radio name="groupOptions">
+                {item.Name} | {item.ApplicationVersion} | {item.CharacterName} | {item.MainHeroLevel} | {item.DayLong} |{' '}
+                {item.CreationTime}
+              </Radio>
+            </div>
+          </FlexLayout.Flex>
+        </FlexLayout>
+      </ListGroupItem>
+    );
+  }
 }
 
 export default SaveListEntry;
