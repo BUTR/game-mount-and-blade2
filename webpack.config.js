d944ed7b54c288fde2b9ed3fea60cfd49280b9e0
Vitalii Mikhailov <personal@aragas.org>
2024-01-12 01:01:44 +0200

    Migrated to node 18
    Added resolutions from Vortex, forcing winapi-bindings to use https instead of ssh
    Fixed WebPack4 issue with SSL

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const webpack = require('vortex-api/bin/webpack');
 const CopyPlugin = require("copy-webpack-plugin");
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm)
 
 const config = webpack.default('game-mount-and-blade2', __dirname, 4);
 config.plugins.push(new CopyPlugin({
