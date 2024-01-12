4a718a08a593ddfbfb041533170f6e4d95136287
Vitalii Mikhailov <personal@aragas.org>
2024-01-12 02:02:00 +0200

    Adapted to webpack 5
    Moved turbowalk and xml2js to dev deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
-const webpack = require('vortex-api/bin/webpack');
-const CopyPlugin = require("copy-webpack-plugin");
-const crypto = require("crypto");
-const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm)
+const webpack = require('vortex-api/bin/webpack').default;
+const CopyPlugin = require('copy-webpack-plugin');
 
-const config = webpack.default('game-mount-and-blade2', __dirname, 4);
+const config = webpack('game-mount-and-blade2', __dirname, 5);
+
 config.plugins.push(new CopyPlugin({
     patterns: [
-        { from: "**/Bannerlord.LauncherManager.Native.dll", to: "[name].[ext]" },
-        { from: "assets/*.jpg", to: "[name].[ext]" },
-        { from: "assets/*.png", to: "[name].[ext]" },
-        { from: "src/stylesheets/*.scss", to: "[name].[ext]" },
-        { from: "assets/localization/**/*.xml", to: "localization_[contenthash].[ext]" },
+        { from: "**/Bannerlord.LauncherManager.Native.dll", to: "[name][ext]" },
+        { from: "assets/*.jpg", to: "[name][ext]" },
+        { from: "assets/*.png", to: "[name][ext]" },
+        { from: "src/stylesheets/*.scss", to: "[name][ext]" },
+        { from: "assets/localization/**/*.xml", to: "localization_[contenthash][ext]" },
     ],
 }));
 config.module.rules.push({
     test: /\.node$/,
-    loader: "node-loader",
+    loader: "native-addon-loader",
     options: {
-        name: '[name].[ext]'
+        name: '[contenthash].[ext]'
+
     }
 });
 module.exports = config;
